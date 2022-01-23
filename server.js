d5e80930ae967a3dd7f3ffd9266fadb9b74f2ab0
Tom Riley <mail@thomasriley.ca>
2022-01-23 22:25:59 +0000

    Update default response

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World Updated');
+  res.send('Article service is running!');
 });
 
 app.listen(PORT, HOST);
