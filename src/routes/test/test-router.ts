569d7f3aeb5fdb631d3d87f58ee62f2b84385ff3
Tom Riley <tomdriley@outlook.com>
2023-03-26 20:03:22 +0000

    Increase code commenting (from GPT-4 mostly)

--- src/routes/test/test-router.ts
+++ src/routes/test/test-router.ts
@@ -1,40 +1,46 @@
+// Import the required modules and dependencies
 import express, { Router, Request, Response } from "express";
-import { getEnv } from "../../utils";
-import axios from "axios";
+import { getEnv, fetchDataFromService } from "../../utils";
 
+// Retrieve the ARTICLE_SERVICE_URI from environment variables
 const ARTICLE_SERVICE_URI = getEnv("ARTICLE_SERVICE_URI");
 
-const getArticleServiceMessage = async (url: string) => {
-  const articleServiceResponse = await axios.get(url);
-  const articleServiceData = articleServiceResponse.data;
-  return articleServiceData;
-};
-
+// Define the testRouter function, which returns an Express router
 const testRouter = (): Router => {
+  // Create a new Express router
   const router = express.Router();
 
+  // Define a route handler for the root path of the testRouter
   router.get("/", async (req: Request, res: Response) => {
     try {
+      // Record the start time of the request to the Article Service
       const startTime: Date = new Date();
-      const articleServiceMessage = await getArticleServiceMessage(
+      // Get the message from the Article Service using the fetchDataFromService function
+      const articleServiceMessage = await fetchDataFromService(
         ARTICLE_SERVICE_URI
       );
+      // Record the end time of the request to the Article Service
       const endTime: Date = new Date();
+      // Calculate the response time of the Article Service request
       const responseTimeMilliseconds = endTime.getTime() - startTime.getTime();
+      // Send a response to the client with the response time and message from the Article Service
       res.send(
-        "The website frontend is running! Article service says (" +
-          responseTimeMilliseconds +
-          " ms): " +
-          articleServiceMessage
+        `The website frontend is running! Article service says (${responseTimeMilliseconds} ms): ${articleServiceMessage}`
       );
     } catch (error) {
-      res.send(
-        "The website frontend is running! Failed to fetch article service"
-      );
+      // If there was an error fetching the message from the Article Service, send an error message to the client
+      console.error("Failed to fetch article service:", error);
+      res
+        .status(500)
+        .send(
+          "The website frontend is running! Failed to fetch article service"
+        );
     }
   });
 
+  // Return the configured router
   return router;
 };
 
+// Export the testRouter function as the default export
 export default testRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
+// Import the Express module for creating a web server
 import express from "express";
+// Import custom router module to handle different routes
 import router from "./routes/router";
 
-// Constants
+// Set the server's listening port
 const PORT = 8080;
+// Set the server's listening IP address (0.0.0.0 listens on all available network interfaces)
 const HOST = "0.0.0.0";
 
-// App
+// Create a new Express app instance
 const app = express();
+// Set the view engine to EJS, which allows embedding JavaScript in HTML templates
 app.set("view engine", "ejs");
+// Mount the custom router on the root path ("/") of the app
 app.use("/", router());
 
+// Start the server, listening on the specified IP address and port
 app.listen(PORT, HOST);
+// Log a message to the console, indicating the server is running and its URL
 console.log(`Running on http://${HOST}:${PORT}`);
